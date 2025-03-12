d08c18cef6b69a9d18a9691760ac6cb8f15b8217
Cody Fisher <fishercody23@gmail.com>
2025-03-11 22:37:32 -0600

    Added the left and right columns as components. Started on a left column component 'contact'

---
+++ src/components/layout/LeftColumn.jsx
@@ -0,0 +1,11 @@
+import Contact from "../resume/contact";
+const LeftColumn = () => {
+    return (
+    <div className="LeftColumn">
+        <img src="/src/assets/profile.jpg" className="profilePicture"/>
+        <Contact/>
+    </div>
+    )
+}
+
+export default LeftColumn;
---
+++ src/components/layout/RightColumn.jsx
@@ -0,0 +1,11 @@
+const RightColumn = ({name, jobTitle}) => {
+    return (
+    <div className="RightColumn">
+        <h1>{name}</h1>
+        <h2>{jobTitle}</h2>
+    </div>
+    )
+    
+}
+
+export default RightColumn;
