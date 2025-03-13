4bcab1d5593a3eaed9bb4314826a8cdb9c8a4ac7
Cody Fisher <fishercody23@gmail.com>
2025-03-12 22:14:59 -0600

    Finshed the Contact section. Added a UI component that allows an editable field. The editable field produces a span with default value that can be edited by the user by replacing the span with a text input, the replaces the text input back with the span with the value of the input.

---
+++ src/components/UI/EditableField.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function EditableField({ initialValue, onSave, className, maxLength, type}) {
+    const [value, setValue] = useState(initialValue);
+    const [editing, setEditing] = useState(false);
+    const handleKeyDown = (e) => {
+
+      if (e.key === 'Enter') {
+
+        onSave(value);
+        setEditing(false);
+      }
+    };
+    
+
+    return (
+      <>
+        {editing ? (
+          <input
+            type={type}
+            className='editableInput'
+            maxLength={maxLength}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            onBlur={() => {
+              onSave(value);
+              setEditing(false);
+            }}
+            autoFocus
+          />
+        ) : (
+          <span onClick={() => setEditing(true)} className={className}>
+            {value}
+          </span>
+        )}
+      </>
+    );
+  }
+
+export default EditableField;
