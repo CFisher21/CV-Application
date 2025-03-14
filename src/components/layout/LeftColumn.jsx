0828dcb85670c72e49b0e558c003a3a1c7fefba9
Cody Fisher <fishercody23@gmail.com>
2025-03-13 20:18:28 -0600

    Added an education section on the left column

--- src/components/layout/LeftColumn.jsx
+++ src/components/layout/LeftColumn.jsx
@@ -1,9 +1,11 @@
 import Contact from "../resume/contact";
+import Education from "../resume/Education"
 const LeftColumn = () => {
     return (
     <div className="LeftColumn">
         <img src="/src/assets/profile.jpg" className="profilePicture"/>
         <Contact/>
+        <Education/>
     </div>
     )
 }
---
+++ src/components/resume/Education.jsx
@@ -0,0 +1,24 @@
+import EditableField from "../UI/EditableField";
+import React, {useState} from 'react';
+
+const Education = () => {
+
+    const className = 'educationGroupSpan'
+    const [school, setSchool] = useState('Keyin College')
+    const [eduYear, setEduYear] = useState('2021 - 2024')
+    const [degree, setDegree] = useState('Software Development')
+    const degreeClass = 'degree'
+
+    return(
+    <div className="education">
+        <h3 className="leftColumnHeading">Education</h3>
+        <div className="eduColumnGroup">
+            <EditableField initialValue={eduYear} onSave={setEduYear} className={className} type={'text'}/>
+            <EditableField initialValue={school} onSave={setSchool} className={className} type={'text'}/>
+            <EditableField initialValue={degree} onSave={setDegree} className={degreeClass} type={'text'} />
+        </div>
+    </div>
+    )
+}
+
+export default Education;
