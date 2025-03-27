9885d02a4641532dcbda1e4448fe398491692807
Cody Fisher <fishercody23@gmail.com>
2025-03-26 22:27:18 -0600

    Name section is up and working.

--- src/components/resume/Name.jsx
+++ src/components/resume/Name.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import EditableField from '../UI/EditableField'
 
 const Name = () => {
-    const [name, setName] = useState('CODY')
+    const [firstName, setFirstName] = useState('CODY');
+    const [lastName, setLastName] = useState('FISHER');
+    const [jobTitle, setJobTitle] = useState('Software Developer');
 
     return( 
         <div className="name">
             <div className="fullName">
-                <EditableField initialValue={name} onSave={setName} className={'firstName'} />
-                <h1 className="lastName">Fisher</h1>
+                <EditableField initialValue={firstName} onSave={setFirstName} className={'firstName'} side={'right-name'} type={'text'} />
+                <EditableField initialValue={lastName} onSave={setLastName} className={'lastName'} side={'right-name'} type={'text'}/>
             </div>
-            <h2 className="jobTitle">Software Developer</h2>
+            <EditableField initialValue={jobTitle} onSave={setJobTitle} className={'jobTitle'} side={'right-jobTitle'} type={'text'}/>
         </div>
     )
 }
