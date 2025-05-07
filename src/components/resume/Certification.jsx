da6deb3aa26e3beba5c0ad670d85ca558772f8c5
Cody Fisher <fishercody23@gmail.com>
2025-05-06 19:18:09 -0600

    Added info to the certification section

--- src/components/resume/Certification.jsx
+++ src/components/resume/Certification.jsx
@@ -1,12 +1,13 @@
+import { useState } from 'react';
 import ExpandableList from '../UI/ExpandableList'
 
 const Certification = () => {
-
+    const certifications = ['AWS Academy Cloud Architecting (2022)', 'AWS Academy Cloud Developing (2022)']
     return(
         <div className="certification">
             <h1 className="certTitle">Certification</h1>
             <hr></hr>
-            <ExpandableList side={'right'} deleteClass={'deleteBtn2'} listItemClass={'certList'} buttonName={'Add Certification'} listItemName={'Certification'} buttonClass={'addCert'}/>
+            <ExpandableList initialValue={certifications}  side={'right'} deleteClass={'deleteBtn2'} listItemClass={'certList'} buttonName={'Add Certification'} listItemName={'Certification'} buttonClass={'addCert'}/>
         </div>
     )
 }
